1ad4b7c4ac99475901d98292be608a2b77d8070d
ahmedehab72 <ahmedehab.eng@gmail.com>
2026-03-02 12:44:33 +0200

    feat(auth): add employer and candidate login/register pages with layout and components

--- src/app/[locale]/auth/(pages)/employer/login/page.tsx
+++ src/app/[locale]/auth/(pages)/employer/login/page.tsx
@@ -1,6 +1,5 @@
 // libraries
 import Link from "next/link";
-import HeaderLoginContent from "../../../components/login-content/HeaderLoginContent";
 import FormEmployerLogin from "../../../components/login-content/FormEmployerLogin";
 import SocialLoginButtons from "../../../components/login-content/SocialLoginButtons";
 
@@ -12,20 +11,23 @@ const LoginEmployerPage = () => {
     <div className="h-[calc(100vh-75px)] flex items-center justify-center gap-4 ">
       <div className="w-3/4 mx-auto p-4">
         {/* header text */}
-        <HeaderLoginContent />
+              <h1>Welcome back,<br /> business.</h1>
+      <p className="text-[clamp(.8rem,4vw,1rem)]">
+        Manage your job posts and candidates
+      </p>
 
         {/* Login form */}
         <FormEmployerLogin />
 
-        {/* social buttons */}
         <div
           className="flex items-center gap-3 my-4 text-sm text-gray-500 font-medium 
-                before:content-[''] before:flex-1 before:h-px before:bg-gray-200
-                after:content-[''] after:flex-1 after:h-px after:bg-gray-200"
-        >
+          before:content-[''] before:flex-1 before:h-px before:bg-gray-200
+          after:content-[''] after:flex-1 after:h-px after:bg-gray-200"
+          >
           or
         </div>
 
+          {/* social buttons */}
         <SocialLoginButtons />
         {/* Bottom CTA */}
         <div className="text-center border-t border-gray-100 pt-6">
