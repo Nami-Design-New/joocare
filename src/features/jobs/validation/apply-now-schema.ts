647b7e72c33b4dda5d42ed5e7489edb64db67c6d
ahmedehab72 <ahmedehab.eng@gmail.com>
2026-05-05 10:23:03 +0300

    feat: reduce maximum file size for CV uploads to 5MB across multiple components

--- src/features/jobs/validation/apply-now-schema.ts
+++ src/features/jobs/validation/apply-now-schema.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 
 export const ApplyNowSchema = z.object({
-  uploadCV: z.any().refine((file) => file, "CV is required and max size is (10MB)"),
+  uploadCV: z.any().refine((file) => file, "CV is required and max size is (5MB)"),
 });
 
 export type TApplyNowSchema = z.infer<typeof ApplyNowSchema>;
