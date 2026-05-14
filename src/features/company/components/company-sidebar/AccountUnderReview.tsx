e397f729ddd15847f2220802f7ab8da4b14cdbee
ahmedehab72 <ahmedehab.eng@gmail.com>
2026-05-14 19:57:06 +0300

    fix: add optional chaining to companyProfileData properties in AccountUnderReview component

--- src/features/company/components/company-sidebar/AccountUnderReview.tsx
+++ src/features/company/components/company-sidebar/AccountUnderReview.tsx
@@ -11,7 +11,7 @@ const AccountUnderReview = ({ companyProfileData }: { companyProfileData: TCompa
         <section className="bg-background border rounded-2xl flex flex-col gap-3 py-3 px-4">
             <div className="flex gap-2 w-full items-center ">
                 <Image
-                    src={companyProfileData.image}
+                    src={companyProfileData?.image}
                     alt={t("companyPage.sidebar.profile-image-alt")}
                     width={60}
                     height={60}
@@ -19,32 +19,32 @@ const AccountUnderReview = ({ companyProfileData }: { companyProfileData: TCompa
                 />
                 <div>
                     <p className="text-black font-semibold text-md">
-                        {companyProfileData.name}
+                        {companyProfileData?.name}
                     </p>
                     <p className="text-md font-normal mt-1">
-                        {companyProfileData.domain.title}
+                        {companyProfileData?.domain?.title}
                     </p>
                 </div>
             </div>
-            {companyProfileData.status === "Pending" && (
+            {companyProfileData?.status === "Pending" && (
                 <p className="text-sm text-muted-foreground text-wrap">
-                    {companyProfileData.bio.slice(0, 100)}
+                    {companyProfileData?.bio?.slice(0, 100)}
                 </p>
             )}
-            {companyProfileData.status === "Rejected" && (
+            {companyProfileData?.status === "Rejected" && (
                 <p className="text-sm text-muted-foreground text-wrap">
-                    {companyProfileData.rejection_reason}
+                    {companyProfileData?.rejection_reason}
                 </p>
             )}
             {
-                companyProfileData.status === "Pending" && (
+                companyProfileData?.status === "Pending" && (
                     <Badge className="bg-warning-bg text-warning justify-start text-base  py-1 px-4 mt-2 w-full font-normal whitespace-normal">
                         {t("companyPage.sidebar.account-under-review")}
                     </Badge>
                 )
             }
             {
-                companyProfileData.status === "Rejected" && (
+                companyProfileData?.status === "Rejected" && (
                     <Badge className="bg-red-50 text-red-500 justify-start text-base  py-1 px-4 mt-2 w-full font-normal whitespace-normal">
                         {t("companyPage.sidebar.account-review-rejected")}
                     </Badge>
